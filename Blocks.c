7c108ca126af202ee60c6cf103e52a0f9fda48dc
LinusL <66841417+Linus0080@users.noreply.github.com>
2022-04-28 15:00:38 +0800

    Add files via upload
    First file with C
    Achieved with unicode `0x2588`

---
+++ Blocks.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
+void block() {
+    setlocale(LC_CTYPE, "");
+    wchar_t block = 0x2588;
+    wprintf(L"%2lc", block);
+}
+int main() {
+    int chart[5][7] = {{1,1,1,1,1,1},{1,1,1,1,1},{1,1,1,1},{1,1,1,1,1,1,1},{1,1}};
+    int i,j;
+    for (i = 0; i < 5; i++)
+    {
+       for ( j = 0; j < 7; j++)
+       {
+           if (chart[i][j] != 0)
+           {
+               block();
+           } 
+       }
+       printf("\n");
+       printf("\n");
+    }
+    return 0;
+}
