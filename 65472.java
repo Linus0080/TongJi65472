6750c711c6d8717762c4755bf849d39b6c24f585
Kitsune <45789471+KyuubiRan@users.noreply.github.com>
2022-04-29 22:48:27 +0800

    Update 65472.java

--- 65472.java
+++ 65472.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i : new int[]{6, 5, 4, 7, 2}) {
-            if (i % 2 == 0) System.out.println("🟥".repeat(i));
-            else System.out.println("🟦".repeat(i));
-        }
+        for (int i : new int[]{6, 5, 4, 7, 2}) System.out.println((i % 2 == 0 ? "🟥" : "🟦").repeat(i));
     }
 }
