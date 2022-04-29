deedc864075c883b9232eb9797715f38b93733d1
Kitsune <45789471+KyuubiRan@users.noreply.github.com>
2022-04-29 22:46:05 +0800

    Update 65472.kt

--- 65472.kt
+++ 65472.kt
@@ -1,3 +1,3 @@
 fun main() {
-    listOf(6, 5, 4, 7, 2).forEach { println(if (it % 2 == 0) "🟥".repeat(it) else "🟦".repeat(it)) }
+    listOf(6, 5, 4, 7, 2).forEach { println((if (it % 2 == 0) "🟥" else "🟦").repeat(it)) }
 }
