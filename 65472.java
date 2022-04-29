c4ba53d3d22274de16e935fad6c31c46bc7934c2
Kitsune <45789471+KyuubiRan@users.noreply.github.com>
2022-04-29 21:44:12 +0800

    Update 65472.java

--- 65472.java
+++ 65472.java
@@ -1,12 +1,10 @@
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        List.of(6, 5, 4, 7, 2).forEach(i -> {
+        for (int i : new int[]{6, 5, 4, 7, 2}) {
             for (int j = 0; j < i; j++)
                 if (i % 2 == 0) System.out.print("\uD83D\uDFE5");
                 else System.out.print("\uD83D\uDFE6");
             System.out.println();
-        });
+        }
     }
 }
