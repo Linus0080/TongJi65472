86ec082d9f8da1d80649c581e8c3156d94a39314
Kitsune <45789471+KyuubiRan@users.noreply.github.com>
2022-04-29 22:42:47 +0800

    Update 65472.java

--- 65472.java
+++ 65472.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         for (int i : new int[]{6, 5, 4, 7, 2}) {
-            for (int j = 0; j < i; j++)
-                if (i % 2 == 0) System.out.print("\uD83D\uDFE5");
-                else System.out.print("\uD83D\uDFE6");
-            System.out.println();
+            if (i % 2 == 0) System.out.println("🟥".repeat(i));
+            else System.out.println("🟦".repeat(i));
         }
     }
 }
