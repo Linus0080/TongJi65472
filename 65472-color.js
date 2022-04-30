17e5d20d309fce26c9fafd8bda2667714b398189
HIMlaoS_Misa <misaliu@misaliu.top>
2022-04-30 17:24:29 +0800

    Update 65472-color.js

--- 65472-color.js
+++ 65472-color.js
@@ -2,5 +2,5 @@ let blocksPerLine = { 0:6, 1:5, 2:4, 3:7, 4:2 };
 for (let i in blocksPerLine) {
     let output = '';
     for (let j = 0; j < blocksPerLine[i]; j++) output += '■';
-	console.log('%c' + output, 'color:' + (i % 2 == 0 ? 'red' : 'blue'));
+    console.log('%c' + output, 'color:' + (i % 2 == 0 ? 'red' : 'blue'));
 }
