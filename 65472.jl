f8fd93299b8d9ebaab4a4e98da6af279e432880b
NekokeCore <Core2002@163.com>
2022-04-30 12:16:33 +0800

    By One Line

--- 65472.jl
+++ 65472.jl
@@ -1,10 +1,1 @@
-for j in [6, 5, 4, 7, 2]
-    for i in range(1, j)
-        if j & 1 == 0
-            print('🟥')
-        else
-            print('🟦')
-        end
-    end
-    println()
-end
+for j in [6, 5, 4, 7, 2] for i in range(1, j) if j & 1 == true print('🟦') else print('🟥') end end ; println() end
